5e0028c498e781f9323a47d6b9aa22ddc747f898
Andrii <60845315+kozopas228@users.noreply.github.com>
2024-12-02 15:05:55 +0200

    Add config, routes, basic header, basic styles

---
+++ frontend/src/pages/pomodoro/PomodoroPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '@/components/header/Header.tsx';
+
+const PomodoroPage = () => {
+    return (
+        <div className='h-screen w-screen bg-amber-50'>
+            <Header />
+        </div>
+    );
+};
+
+export default PomodoroPage;
