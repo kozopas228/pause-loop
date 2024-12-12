55ed8569a44345683f393518571a92daee4a89a0
Andrii <60845315+kozopas228@users.noreply.github.com>
2024-12-12 16:52:18 +0200

    add inhale and exhale sounds, inhale holding breath countdown, finish page and dark theme

--- frontend/src/pages/breathing/BetweenRoundCountdown.tsx
+++ frontend/src/pages/breathing/BetweenRoundCountdown.tsx
@@ -6,13 +6,15 @@ import { motion } from 'framer-motion';
 
 interface IProps {
     setPhase: Dispatch<SetStateAction<BreathingPhaseEnum>>;
+    setRound: Dispatch<SetStateAction<number>>;
 }
 
-const PreparationCountdown = ({ setPhase }: IProps) => {
+const BetweenRoundCountdown = ({ setPhase, setRound }: IProps) => {
     const [startTime, setStartTime] = useState(Date.now());
 
     function handleOnComplete() {
         setPhase(BreathingPhaseEnum.Breathing);
+        setRound((prev) => prev + 1);
     }
 
     function renderer(props: CountdownRenderProps): ReactNode {
@@ -21,15 +23,15 @@ const PreparationCountdown = ({ setPhase }: IProps) => {
                 <div
                     className={`z-0 h-[512px] w-[512px] rounded-full bg-blue-200 dark:bg-sky-700`}>
                     <motion.div
-                        className='absolute left-[50%] top-[50%] flex translate-x-[-50%] translate-y-[-50%] flex-col items-center justify-center'
+                        className='absolute left-[50%] top-[46%] flex translate-x-[-50%] translate-y-[-50%] flex-col items-center justify-center'
                         initial={{ opacity: 0 }}
                         animate={{ opacity: 1 }}
                         exit={{ opacity: 0 }}
                         transition={{ duration: 0.5 }}>
                         <p className='text-lg font-semibold text-sky-900 dark:text-sky-300'>
-                            Be ready to inhale in:
+                            Next Round in:
                         </p>
-                        <div className='mt-2 flex justify-center text-8xl font-extrabold text-sky-700 dark:text-sky-300'>
+                        <div className='mt-2 flex justify-center text-5xl font-extrabold text-sky-700 dark:text-sky-300'>
                             {props.formatted.minutes}:{props.formatted.seconds}
                         </div>
                     </motion.div>
@@ -50,4 +52,4 @@ const PreparationCountdown = ({ setPhase }: IProps) => {
     );
 };
 
-export default PreparationCountdown;
+export default BetweenRoundCountdown;
