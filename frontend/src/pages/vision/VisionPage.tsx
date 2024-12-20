1b304a931a1d41d90179d8b2dd39d6caf69378c3
Andrii <60845315+kozopas228@users.noreply.github.com>
2024-12-20 14:18:11 +0200

    add what is this section to vision page

--- frontend/src/pages/vision/VisionPage.tsx
+++ frontend/src/pages/vision/VisionPage.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
-import { ChevronDown, ChevronUp } from 'lucide-react';
+import { ChevronDown, ChevronUp, RotateCcw } from 'lucide-react';
 import BodyBackground from '@/components/body-background/BodyBackground.tsx';
 import Page from '@/components/page/Page.tsx';
 import VisionTimer from '@/pages/vision/VisionTimer.tsx';
+import TestImage from '@/assets/images/brown-noise-background.jpg';
+import VisionWorkingImage from '@/assets/vectors/vision-working.svg?react';
+import VisionLookAway from '@/assets/vectors/vision-look-away.svg?react';
 
 const VisionPage = () => {
     const [isShowDescription, setIsShowDescription] = useState(false);
@@ -35,13 +38,39 @@ const VisionPage = () => {
                 </p>
             </div>
             {isShowDescription && (
-                <div className='mt-4 flex justify-center text-center'>
-                    <p className='w-3/4 text-emerald-900 dark:text-emerald-50'>
-                        The 20/20/20 technique is a method to reduce eye strain
-                        while working on a computer. Every 20 minutes, take a
-                        20-second break and look at an object 20 feet (6 meters)
-                        away to help reset focus and relieve eye fatigue.
-                    </p>
+                <div className='mb-12 mt-4'>
+                    <h3 className='text-center text-lg font-bold text-emerald-900 dark:text-emerald-50'>
+                        To reduce eye strain caused by prolonged screen time:
+                    </h3>
+                    <div className='mx-auto mt-12 grid auto-cols-auto grid-cols-1 items-start justify-items-center gap-4 gap-y-20 md:mt-20 md:grid-cols-2 lg:w-3/4 lg:grid-cols-3 2xl:w-1/2'>
+                        <div className='flex h-auto w-64 flex-col items-center'>
+                            <div className='flex h-10 w-10 items-center justify-center rounded-full bg-black text-lg font-bold text-white dark:bg-white dark:text-black'>
+                                1
+                            </div>
+                            <p className='mt-6 text-center font-semibold'>
+                                Work for 20 minutes
+                            </p>
+                            <VisionWorkingImage className='mt-6 h-auto w-full md:mt-12' />
+                        </div>
+                        <div className='flex h-auto w-64 flex-col items-center'>
+                            <div className='flex h-10 w-10 items-center justify-center rounded-full bg-black text-lg font-bold text-white dark:bg-white dark:text-black'>
+                                2
+                            </div>
+                            <p className='mt-6 text-center font-semibold'>
+                                Look at something 20 feet away for 20 seconds
+                            </p>
+                            <VisionLookAway className='mt-6 h-auto w-full md:mt-12' />
+                        </div>
+                        <div className='flex h-auto w-64 flex-col items-center'>
+                            <div className='flex h-10 w-10 items-center justify-center rounded-full bg-black text-lg font-bold text-white dark:bg-white dark:text-black'>
+                                3
+                            </div>
+                            <p className='mt-6 text-center font-semibold'>
+                                Repeat
+                            </p>
+                            <RotateCcw className='mt-6 h-auto w-32 stroke-emerald-800 stroke-2 dark:stroke-emerald-50 md:mt-12' />
+                        </div>
+                    </div>
                 </div>
             )}
 
