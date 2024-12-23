2725ca5e8fc80ce7bc74fc36d0f098997a384bb5
Andrii <60845315+kozopas228@users.noreply.github.com>
2024-12-23 11:39:22 +0200

    add brown noise page light theme

--- frontend/src/pages/noise/NoisePage.tsx
+++ frontend/src/pages/noise/NoisePage.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import Page from '@/components/page/Page.tsx';
 import BodyBackground from '@/components/body-background/BodyBackground.tsx';
 import BrownNoiseBackgroundImage from '@/assets/images/brown-noise-background.jpg';
+import BrownNoiseBackgroundImageLight from '@/assets/images/brown-noise-background-light.jpg';
 import CustomPlayIcon from '@/assets/vectors/custom-play.svg?react';
 import CustomPauseIcon from '@/assets/vectors/custom-pause.svg?react';
 import BrownNoise from '@/assets/audio/brown-noise-fast.wav';
@@ -9,6 +10,7 @@ import BrownNoise from '@/assets/audio/brown-noise-fast.wav';
 import { Slider } from '@/shadcn/components/ui/slider.tsx';
 import BrownNoiseGenerator from '@/pages/noise/BrownNoiseGenerator.tsx';
 import { ChevronDown, ChevronUp, Volume2 } from 'lucide-react';
+import { useTheme } from '@/shadcn/components/theme-provider.tsx';
 
 const NoisePage = () => {
     const [isPlaying, setIsPlaying] = useState(false);
@@ -23,10 +25,13 @@ const NoisePage = () => {
         setIsShowDescription((prev) => !prev);
     };
 
+    const theme = useTheme();
+    console.log(theme);
+
     return (
         <Page
             style={{
-                backgroundImage: `url(${BrownNoiseBackgroundImage}), radial-gradient(circle, transparent 60%, rgba(0, 0, 0, 0.5) 100%)`,
+                backgroundImage: `${theme.resolvedTheme === 'light' ? `url(${BrownNoiseBackgroundImageLight})` : `url(${BrownNoiseBackgroundImage}), radial-gradient(circle, transparent 60%, rgba(0, 0, 0, 0.5) 100%)`} `,
                 backgroundPosition: 'center center',
                 backgroundSize: 'cover',
                 backgroundBlendMode: 'overlay',
@@ -45,9 +50,7 @@ const NoisePage = () => {
                 )}
 
                 <div className='mt-2 flex w-1/2 opacity-50 hover:opacity-100 md:mt-6 md:w-1/4'>
-                    <Volume2
-                        className='mr-4 stroke-white h-12 w-12 md:w-16 md:h-16'
-                    />
+                    <Volume2 className='mr-4 h-12 w-12 stroke-foreground md:h-16 md:w-16' />
                     <Slider
                         min={0}
                         max={20}
@@ -62,27 +65,27 @@ const NoisePage = () => {
                 <div className='w-3/4'>
                     <div className='my-8 cursor-pointer opacity-50 hover:opacity-100'>
                         <p
-                            className='flex items-center justify-center text-sm font-medium text-orange-50'
+                            className='flex items-center justify-center text-sm font-medium text-orange-950 dark:text-orange-50'
                             onClick={handleDescriptionClick}>
                             What is this?
                             {isShowDescription ? (
                                 <ChevronUp
                                     width={25}
                                     height={25}
-                                    className='ml-1 inline-block stroke-orange-50'
+                                    className='ml-1 inline-block stroke-orange-950 dark:stroke-orange-50'
                                 />
                             ) : (
                                 <ChevronDown
                                     width={25}
                                     height={25}
-                                    className='ml-1 inline-block stroke-orange-50'
+                                    className='ml-1 inline-block stroke-orange-950 dark:stroke-orange-50'
                                 />
                             )}
                         </p>
                     </div>
                     {isShowDescription && (
                         <div className='mt-4 flex justify-center text-center'>
-                            <p className='text-orange-50'>
+                            <p className='text-orange-950 dark:text-orange-50'>
                                 Brown noise is a type of sound that has low
                                 frequencies and a deep, rumbling quality. It
                                 sounds like distant thunder or a heavy
@@ -100,7 +103,7 @@ const NoisePage = () => {
                 volume={volume}
             />
 
-            <BodyBackground className={'bg-orange-950'} />
+            <BodyBackground className={'bg-orange-50 dark:bg-orange-950'} />
         </Page>
     );
 };
